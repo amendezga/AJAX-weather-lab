3b6d6537823ec75925e9f4c16484b9cc56c8ae0e
Alejandro <amendez1361@bths.edu>
2022-12-18 22:11:52 -0500

    added lat lon function

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const $location = $('#location');
+const $temp = $('#temp');
+const $feels = $('#feels');
+const $weather = $('#weather');
+const $input = $('input[type="text"]');
+let userInput;
+
+$("form").on("submit", handleGetData)
+
+function handleGetData(event) {
+    event.preventDefault()
+    userInput = $input.val();
+    $.ajax({
+        url: `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=8f795061fa1c7a09f92221bad59a9faa`
+    }).then(
+        (data) => {
+            lat = data[0]['lat']
+            lon = data[0]['lon']
+            apply();
+        },
+        (error) => {
+            console.log("bad request", error)
+        }
+    )
+}
